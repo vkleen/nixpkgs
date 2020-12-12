b40ce13e78171375fe0f37dc1c1fc463c5e4b094
Viktor Kleen <viktor@kleen.org>
2020-12-12 15:43:55 +0000

    HACK: disable qemu docs

--- pkgs/applications/virtualization/qemu/default.nix
+++ pkgs/applications/virtualization/qemu/default.nix
@@ -122,7 +122,9 @@ stdenv.mkDerivation rec {
 
   configureFlags =
     [ "--audio-drv-list=${audio}"
-      "--enable-docs"
+      "--disable-docs"
+      "--sysconfdir=/etc"
+      "--localstatedir=/var"
       "--enable-tools"
       "--enable-guest-agent"
       "--localstatedir=/var"
